77e360d068bc4634c16de577ed21bea423792411
0x003e <0x003e@gmail.com>
2020-04-28 05:59:50 +0200

    Upgrade Minio Admin API v2 -> v3 since RELEASE.2020-04-10T03-34-42Z: https://github.com/minio/minio/commit/2642e12d14fe6a3af53d46b0996603a6786a988b#diff-f84da3b3655cf26658302b676b19873b

--- src/handlers/policy.go
+++ src/handlers/policy.go
@@ -1,14 +1,17 @@
 package handlers
 
 import (
+	"context"
 	iris "github.com/kataras/iris/v12"
+	iampolicy "github.com/minio/minio/pkg/iam/policy"
 	resph "github.com/rzrbld/adminio-api/response"
 	log "log"
 	strconv "strconv"
+	"strings"
 )
 
 var PolList = func(ctx iris.Context) {
-	lp, err := madmClnt.ListCannedPolicies()
+	lp, err := madmClnt.ListCannedPolicies(context.Background())
 	var res = resph.BodyResHandler(ctx, err, lp)
 	ctx.JSON(res)
 }
@@ -19,7 +22,10 @@ var PolAdd = func(ctx iris.Context) {
 	p.policyString = ctx.FormValue("policyString")
 
 	if resph.CheckAuthBeforeRequest(ctx) != false {
-		err = madmClnt.AddCannedPolicy(p.policyName, p.policyString)
+		policy, err := iampolicy.ParseConfig(strings.NewReader(p.policyString))
+		if err == nil {
+			err = madmClnt.AddCannedPolicy(context.Background(), p.policyName, policy)
+		}
 		var res = resph.DefaultResHandler(ctx, err)
 		ctx.JSON(res)
 	} else {
@@ -32,7 +38,7 @@ var PolDelete = func(ctx iris.Context) {
 	p.policyName = ctx.FormValue("policyName")
 
 	if resph.CheckAuthBeforeRequest(ctx) != false {
-		err = madmClnt.RemoveCannedPolicy(p.policyName)
+		err = madmClnt.RemoveCannedPolicy(context.Background(), p.policyName)
 		var res = resph.DefaultResHandler(ctx, err)
 		ctx.JSON(res)
 	} else {
@@ -54,7 +60,7 @@ var PolSet = func(ctx iris.Context) {
 	}
 
 	if resph.CheckAuthBeforeRequest(ctx) != false {
-		err = madmClnt.SetPolicy(p.policyName, p.entityName, isGroupBool)
+		err = madmClnt.SetPolicy(context.Background(), p.policyName, p.entityName, isGroupBool)
 		var res = resph.DefaultResHandler(ctx, err)
 		ctx.JSON(res)
 	} else {
